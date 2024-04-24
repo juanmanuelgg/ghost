c8054be4bc4e4417beaa4e2242c656b1d995f472
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2024-04-23 19:29:03 -0500

    Primera vez que me sirve puppeteer.

--- javascript/project/modules/e2e-puppeteer/src/ghost/uniandes-example.mjs
+++ javascript/project/modules/e2e-puppeteer/src/ghost/uniandes-example.mjs
@@ -7,18 +7,29 @@ class UniandesExample extends GhostTest {
   }
 
   async testedFunctionality() {
-    const browser = await launch({headless: 'true'});
+    let selector;
+
+    const browser = await launch({
+      args: ["--no-sandbox"],
+      headless: true,
+      timeout: 60000,
+    });
 
     const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(60000);
+    page.setDefaultTimeout(60000);
     await page.goto(
-      "https://angular-6-registration-login-example.stackblitz.io/register",
+      "https://angular-6-registration-login-example.stackblitz.io/register"
     );
 
     await new Promise((r) => setTimeout(r, 7000));
     await page.screenshot({
       path: `screenshots/${this.scriptName}/${this.ghostVersion}/pagina.png`,
     });
-    await page.click("button");
+
+    selector = "button";
+    await page.waitForSelector(selector);
+    await page.click(selector);
     await new Promise((r) => setTimeout(r, 9000));
     await page.screenshot({
       path: `screenshots/${this.scriptName}/${this.ghostVersion}/pagina2.png`,
@@ -26,13 +37,17 @@ class UniandesExample extends GhostTest {
     console.log("Project loaded");
 
     //Interactuar con la aplicación web
-    await page.click("a.btn.btn-link");
-    console.log(`Clicked "cancel". URL is now ${page.url()}`);
+    selector = "a.btn.btn-link";
+    await page.waitForSelector(selector);
+    await page.click(selector),
+      console.log(`Clicked "cancel". URL is now ${page.url()}`);
 
-    await page.click("a.btn.btn-link");
+    selector = "a.btn.btn-link";
+    await page.click(selector);
     console.log(`Clicked "register". URL is now ${page.url()}`);
 
-    await page.click("button.btn.btn-primary");
+    selector = "button.btn.btn-primary";
+    await page.click(selector);
     let feedback = await page.$$("div.invalid-feedback");
 
     let elems = 0;
@@ -43,14 +58,15 @@ class UniandesExample extends GhostTest {
       path: `screenshots/${this.scriptName}/${this.ghostVersion}/form-feedback.png`,
     });
     console.log(
-      `Clicked "Register" with an empty form. Feedback is shown in ${elems} elements`,
+      `Clicked "Register" with an empty form. Feedback is shown in ${elems} elements`
     );
 
     await page.type('input[formcontrolname="firstName"]', "Monitor");
     await page.type('input[formcontrolname="lastName"]', "Pruebas");
     await page.type('input[formcontrolname="username"]', "pruebas");
     await page.type('input[formcontrolname="password"]', "MISO4208");
-    await page.click("button.btn.btn-primary");
+    selector = "button.btn.btn-primary";
+    await page.click(selector);
 
     await new Promise((r) => setTimeout(r, 7000));
     await page.screenshot({
@@ -63,7 +79,8 @@ class UniandesExample extends GhostTest {
 
     await page.type('input[formcontrolname="username"]', "pruebas");
     await page.type('input[formcontrolname="password"]', "MISO4208");
-    await page.click("button.btn.btn-primary");
+    selector = "button.btn.btn-primary";
+    await page.click(selector);
     await new Promise((r) => setTimeout(r, 7000));
 
     feedback = await page.$("h1");
@@ -75,7 +92,7 @@ class UniandesExample extends GhostTest {
     console.log(
       `Logged in. Your user was ${
         text === "Hi Monitor!" ? "successfully" : "not"
-      } created`,
+      } created`
     );
 
     await browser.close();
